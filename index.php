d1a4e6a8a080126d007cc1b93b7d45a6a13c56f9
Dmitry Kuguchev <kuguchev.dm@gmail.com>
2022-02-16 14:08:38 +0700

    Наследование, полиморфизм, цепочки методов, паттерн Builder, array_map

--- index.php
+++ index.php
@@ -1,181 +1,95 @@
 <?php
 
+// Общее у тегов: название и атрибут
+// Любой тег можно отобразить
 
-// Полиморфизм - это изменчивость детей
+class Tag {
+    protected string $name;
+    protected array $attrs = [];
 
-
-// Базовый класс
-class Animal
-{
-    public string $name;
-    public int $health;
-    private int $power;
-    public bool $alive;
-
-    public function __construct(string $name, int $health, int $power)
+    public function __construct(string $name)
     {
         $this->name = $name;
-        $this->health = $health;
-        $this->power = $power;
-        $this->alive = true;
     }
 
-    public function calcDamage()
+    public function attr(string $name, string $value) : Tag
     {
-        return $this->power * (mt_rand(1, 3) / 2);
+        $this->attrs[$name] = $value;
+        return $this; // Аля паттерн builder
     }
 
-    public function applyDamage(int $damage)
+    public function render() : string
     {
-        $this->health -= $damage;
-        if($this->health <= 0) {
-            $this->health = 0;
-            $this->alive = false;
+        return '';
+    }
+
+    protected function attrsToString() : string
+    {
+        $pairs = [];
+
+        foreach($this->attrs as $name => $value) {
+            $pairs[] = "{$name}=\"{$value}\"";
         }
+        $attrsStr = implode(' ', $pairs);
+
+        return (!empty($attrsStr)) ? ' ' . $attrsStr : '';;
     }
 }
 
-// Классы наследники
-
-class Dog extends Animal
+class SingleTag extends Tag
 {
+    public function render() : string
+    {
+        $attrsStr = $this->attrsToString();
 
+        return "<{$this->name}{$attrsStr}>";
+    }
 }
 
-class Cat extends Animal
+class PairTag extends Tag
 {
-    private $lifes;
-    private $baseHealth;
+    protected array $children = [];
 
-    public function __construct(string $name, int $health, int $power)
+    public function appendChild(Tag $child) : PairTag
     {
-        // parent - обращение к родительскому классу
-        // self - обращение к текущему классу
-        parent::__construct($name, $health, $power);
-        $this->lifes = 9;
-        $this->baseHealth = $health;
+        $this->children[] = $child;
+        return $this; // Аля паттерн builder
     }
 
-    public function applyDamage(int $damage)
+    public function render() : string
     {
-        $this->health -= $damage;
-        parent::applyDamage($damage);
-        // Воскрешение кота:)
-        if(!$this->alive && $this->lifes > 1) {
-            $this->lifes--;
-            $this->alive = true;
-            $this->health = $this->baseHealth;
-        }
-    }
-}
+        $attrsStr = $this->attrsToString();
 
-class Mouse extends Animal
-{
-    private $hiddenLevel;
+        // Простой способ добавления дочерних тегов
+//        $innerHTML = '';
+//
+//        foreach ($this->children as $child) {
+//            $innerHTML .= $child->render();
+//        }
 
-    // Количество параметров в конструкторе должно быть одинаковым
-    // что у родителя, что у наследника
-    // Конструктор должен быть с одинаковым интерфейсом
-    public function __construct(string $name, int $health, int $power)
-    {
-        // parent - обращение к родительскому классу
-        // self - обращение к текущему классу
-        parent::__construct($name, $health, $power);
-        $this->hiddenLevel = 0.95;
-    }
-
-    public function setHiddenLevel(float $level)
-    {
-        $this->hiddenLevel = $level;
-    }
-
-    // Переопределение метода родительского класса
-    // Яркий пример полиморфизма (изменчивость)
-    public function applyDamage(int $damage)
-    {
-        // Условие, когда проходит урон по мышке:)
-       if((mt_rand(1, 100) / 100) > $this->hiddenLevel) {
-           parent::applyDamage($damage);
-       }
+        // Продвинутый способ добавления дочерних тегов
+        // Анонимная функция применяется к каждому элементу массива
+        $innerHTML = array_map(function (Tag $tag){
+            return $tag->render();
+        }, $this->children);
+        $innerHTML = implode('', $innerHTML);
+        return "<{$this->name}{$attrsStr}>{$innerHTML}<{$this->name}/>";
     }
 }
 
-class GameCore
-{
-    private array $units; // Массив юнитов
+// Цепочка вызовов методов
+// После применения метода attr возвращается ссылка на текущий объект
+// соответственно мы также можем обратиться к этому же объекту
 
-    // Конструктор создает массив юнитов
-    public function __construct()
-    {
-        $this->units = [];
-    }
+$img = (new SingleTag('img'))
+    ->attr('src', './nz.jpg')
+    ->attr('alt', 'nz not found');
 
-    // Добавление юнита на игровое поле
-    // Явно указываем класс Animal
-    // Чтобы юниты были только из базового класса Animal
-    public function addUnit(Animal $unit)
-    {
-        $this->units[] = $unit;
-    }
+$hr = new SingleTag('hr');
 
-    public function run()
-    {
-        $i = 0;
-        while(count($this->units) > 1)
-        {
-            echo "Round $i <br>";
-            $this->nextTick();
-            $i++;
-            echo '<hr>';
-        }
-    }
+$a = (new PairTag('a'))
+    ->attr('href', './nz')
+    ->appendChild($img)
+    ->appendChild($hr);
 
-    // Следующий ход
-    public function nextTick()
-    {
-        foreach ($this->units as $unit) {
-            // Не знаем, есть ли такой метод у объекта
-            // Возможно обращение к несуществующемому методу объекта
-
-            // Тут непонятно, у какого объекта мы вызываем метод, но
-            // он есть, это может быть родительский класс, либо дочерний
-            // яркий пример полиморфизма
-            $damage = $unit->calcDamage();
-            $target = $this->getRandomUnit($unit);
-            $targetPrevHealth = $target->health;
-            $target->applyDamage($damage);
-            echo "{$unit->name} beat {$target->name}, damage={$damage}, health={$targetPrevHealth} -> 
-                {$target->health} <br>";
-        }
-
-        $this->units = array_values(array_filter($this->units, function ($unit) {
-            return $unit->alive;
-        }));
-    }
-
-    private function getRandomUnit(Animal $exclude)
-    {
-        // Исключение текущего юнита из массива
-        // $units в данном случае просто ссылка на те же самые объекты
-        // ОЗУ не расходуется, новые объекты не создаются
-        // после применения array_filter ключи массива заново не индексируются!
-        //
-        $units = array_values(array_filter($this->units, function ($unit) use ($exclude) {
-            // объекты сравниваются по ссылке
-            return $unit !== $exclude;
-        }));
-
-        return $units[mt_rand(0, count($units) - 1)];
-    }
-}
-
-$core = new GameCore();
-
-$core->addUnit(new Cat('Murzik', 20, 5));
-$core->addUnit(new Dog('Bobik', 200, 10));
-$core->addUnit(new Mouse('Jerry', 10, 2));
-$core->addUnit(new Cat('Garfild', 30, 15));
-$core->addUnit(new Dog('Volk', 180, 9));
-$core->addUnit(new Mouse('Guffy', 10, 5));
-
-$core->run();
+echo $a->render();
