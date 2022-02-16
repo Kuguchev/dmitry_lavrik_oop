39d1ee9a41fc92a447287b58379f3109da677277
Dmitry Kuguchev <kuguchev.dm@gmail.com>
2022-02-16 22:27:38 +0700

    Позднее статическое связывание

--- index.php
+++ index.php
@@ -1,156 +1,62 @@
 <?php
 
-// Только публичные методы без тела
-// Либо константы (редко)
-interface IRenderable
-{
-    public function render() : string;
-}
-
-interface IValidable
-{
-    public function isValid() : bool;
-}
-
-
-// У интерфейсов может быть множественное наследование!
-// Создаем интерфейс INode, который перенимает методы
-// интерфейсов IRenderable, IValidable
-
-interface INode extends IRenderable, IValidable
-{
-
-}
-
-abstract class Tag implements INode
-{
+abstract class Tag{
     protected string $name;
     protected array $attrs = [];
-    protected array $allowedNames;
 
-    public function __construct(string $name)
-    {
+    public function __construct(string $name){
         $this->name = $name;
     }
 
-    // Можно не указывать абстрактный метод render
-    // Это бремя ложится на дочерние классы, т.к. это абстрактный класс
-    // и его экземпляр мы создавать не можем, поэтому определение метода и ложится
-    // на дочерние классы...
-    // abstract public function render() : string;
-
-    public function attr(string $name, string $value)
+    public function attr(string $name, string $value) : static
     {
         $this->attrs[$name] = $value;
         return $this;
     }
 
-    protected function attrsToString() : string
-    {
+    abstract public function render() : string;
+
+    protected function attrsToString() : string{
         $pairs = [];
 
         foreach($this->attrs as $name => $value){
             $pairs[] = "$name=\"$value\"";
         }
-        $attrsStr = implode(' ', $pairs);
-        return (!empty($attrsStr)) ? ' ' . $attrsStr : '';
-    }
 
-    // Метод доступен всем наследникам!
-    // Мы его переопределили в абстрактном классе, поэтому нет необходимости
-    // его переопределять в классах наследниках
-    public function isValid(): bool
-    {
-        return in_array($this->name, $this->allowedNames);
+        return implode(' ', $pairs);
     }
 }
 
-class SingleTag extends Tag
-{
-    protected array $allowedNames = ['img', 'hr'];
-
-    public function render() : string
-    {
+class SingleTag extends Tag{
+    public function render() : string{
         $attrsStr = $this->attrsToString();
-        return "<{$this->name}$attrsStr>";
+        return "<{$this->name} $attrsStr>";
     }
 }
 
-class PairTag extends Tag
-{
-    protected array $allowedNames = ['div', 'span', 'a'];
+class PairTag extends Tag{
     protected array $children = [];
 
-    // Передача в функцию типа IRenderable
-    // позволяет связывать классы, которые находятся не в одной иерархии!
-    // Например можно передать объект класса TextNode, PairTag или SingleTag
-    public function appendChild(INode $child)
-    {
+    public function appendChild(Tag $child) : static{
         $this->children[] = $child;
         return $this;
     }
 
-    public function render() : string
-    {
+    public function render() : string{
         $attrsStr = $this->attrsToString();
 
-        // Добавив интерфейс INode
-        // мы сможем получить доступ к методу render и isValid!
-        // у тех объектов у которых реалихован интерфейс INode
-        $childrenHTML = array_map(function(INode $tag)
-        {
-            // Например можно проверить теперь валидность тега
-            return $tag->isValid() ? $tag->render() : '';
+        $childrenHTML = array_map(function(Tag $tag){
+            return $tag->render();
         }, $this->children);
 
         $innerHTML = implode('', $childrenHTML);
-        return "<{$this->name}$attrsStr>$innerHTML</{$this->name}>";
-    }
-}
-
-class TextNode implements INode
-{
-    protected string $text;
-
-    public function __construct($text)
-    {
-        $this->text = trim($text);
-    }
-
-    public function render() : string
-    {
-        return $this->text;
-    }
-
-    public function isValid(): bool
-    {
-        return $this->text !== '';
-    }
-}
-
-class RandomSomething implements INode
-{
-    public function render(): string
-    {
-        return mt_rand(1, 1000000);
-    }
-
-    public function isValid(): bool
-    {
-        return true;
+        return "<{$this->name} $attrsStr>$innerHTML</{$this->name}>";
     }
 }
 
 $img = (new SingleTag('img'))->attr('src', 'f1.jpg')->attr('alt', 'f1 not found');
-$a = (new PairTag('a'))->attr('href', '#')->appendChild(new TextNode('go home'));
-
 $label = (new PairTag('label'))
-    ->appendChild($img)
-    ->appendChild(new TextNode('attention'))
-    ->appendChild($a)
-    ->appendChild(new RandomSomething());
+    ->attr('class', 'some')
+    ->appendChild($img);
 
-$html = $label->render();
-
-echo $html;
-echo '<hr>' . htmlspecialchars($html);
+echo $label->render();
